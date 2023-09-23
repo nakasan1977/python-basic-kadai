aa0c37aa88c80453b6da83494010bb8d82ca4207
nakasan1977 <144198476+nakasan1977@users.noreply.github.com>
2023-09-24 08:51:43 +0900

    Update kadai_11.py

--- kadai_11.py
+++ kadai_11.py
@@ -1,15 +1,1 @@
-list_1 = ["水","金","地","火","木","土","天","海","冥"]
-i = 1
-for i in range (2):
-  for planet in list_1:
-    
-      print(planet)
-i = i + 1      
-
-list_2 = ["水","金","地","火","木","土","天","海","冥"]
-
-i = 0
-while i < 2:
-  for planet in list_2:
-    print(planet)
-  i = i+1 
+全削除

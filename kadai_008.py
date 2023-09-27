062d9c1fea38f873ca834dbed2cedd05aeab9567
nakasan1977 <144198476+nakasan1977@users.noreply.github.com>
2023-09-27 16:44:22 +0900

    Add files via upload

---
+++ kadai_008.py
@@ -0,0 +1,15 @@
+# 変数varが3の倍数の場合はFizz、5の倍数の場合はBuzz、
+# 3と5の倍数の場合はFizzBuzz、それ以外の場合は変数varを出力する条件式を記述して
+# ください。
+# ただし、変数varは正の整数とします。
+
+var = int(11)
+
+if var % 3 ==0:
+    print("Fizz")
+elif var % 5 ==0:
+    print("Buzz")
+elif var%3 == 0 and var%5 == 0:
+    print("FizzBazz")
+else:
+    print(var)
